11f4bf9c0fdfc6a25dd917a796d4df87bcef8b77
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-09-01 17:59:46 +0330

    page

--- layouts/Default.js
+++ layouts/Default.js
@@ -1,8 +1,6 @@
 import { markdownify } from "@lib/utils/textConverter";
 import { MDXRemote } from "next-mdx-remote";
 import shortcodes from "./shortcodes/all";
-import MustRead from "@layouts/components/MustRead";
-
 
 const Default = ({ data }) => {
   const { frontmatter, mdxContent } = data;
@@ -13,7 +11,6 @@ const Default = ({ data }) => {
         {markdownify(title, "h1", "h2 mb-8 text-center")}
         <div className="content">
           <div>Here</div>
-          <MustRead/>
           <MDXRemote {...mdxContent} components={shortcodes} />
         </div>
       </div>
