6adcc8247f88b77deba3fb6d0f79b9dc5fe503e5
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-09-01 19:16:37 +0330

    page

--- layouts/About.js
+++ layouts/About.js
@@ -1,3 +1,4 @@
+'use client'
 import Social from "@components/Social";
 import { markdownify } from "@lib/utils/textConverter";
 import shortcodes from "@shortcodes/all";
@@ -8,7 +9,43 @@ const About = ({ data }) => {
   const { frontmatter, mdxContent } = data;
   const { title, image, social } = frontmatter;
 
-  return (
+  useEffect(() => {
+    const rssUrl = 'https://www.gamespot.com/feeds/news';
+
+    // Define your POST request parameters (if needed)
+    const postData = {
+      // You can include any necessary data here if required by the RSS service
+    };
+
+    // Define the options for the fetch request
+    const requestOptions = {
+      method: 'POST',
+      // headers: {
+      //   // Add any headers if required
+      //   'Content-Type': 'application/json',
+      // },
+      body: JSON.stringify(postData), // You can convert postData to the desired format
+    };
+
+    // Make the POST request using fetch
+    fetch(rssUrl, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response as JSON
+      })
+      .then((data) => {
+        // Log the response data to the console
+        console.log('Response Data:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }, []);
+    
+    
+      return (
     <section className="section">
       <div className="container text-center">
         {image && (
