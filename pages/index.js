b4205cd4e42797eb4afe30301379f16be8499790
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-03 14:20:30 +0330

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,3 @@
-// pages/index.js
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import MustRead from "@layouts/components/MustRead";
-
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>My Next.js Page</title>
-      </Head>
-      <h1>Welcome to Next.js!</h1>
-          <MustRead/>
-
-      <p>This is a simple example of a Next.js page.</p>
-      <Link href="/about">
-        <a>Go to About page</a>
-      </Link>
-    </div>
-  )
-}
+import BlogPagination, { getStaticProps } from "./page/[slug]";
+export { getStaticProps };
+export default BlogPagination;
