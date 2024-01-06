874f47474746634a55cef39fc366934a921954ac
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-06 15:54:08 +0330

    commit

--- layouts/components/MustRead.js
+++ layouts/components/MustRead.js
@@ -7,151 +7,12 @@ import {
 
 import xml2js from 'xml2js';
 
-// Define a custom component called MustRead that takes an array of articles as props
 const MustRead = ({ articles }) => {
-  const [firstItemTitle, setFirstItemTitle] = useState('');
-  const [firstItemPost, setFirstItemPost] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/rss');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const xmlData = await response.text(); // Parse the response as text
-  
-      // Parse the XML data into JSON
-      xml2js.parseString(xmlData, (err, result) => {
-        if (err) {
-          throw new Error('Error parsing XML');
-        }
-  
-        // Extract the title of the first item
-        const firstItem = result.rss.channel[0].item[0];
-
-        console.log('first:', JSON.stringify(firstItem, null, 2));
-
-        const title = firstItem.title[0];
-        const postDescription = firstItem.description[0];
-        console.log('first: ', postDescription);
-  
-        // Set the title in the component state
-  
-        // Call the run function after setting the title
-        run(title);
-        runPost(postDescription);
-
-      });
-    };
-    const MODEL_NAME = "gemini-pro";
-    const API_KEY = "AIzaSyASVdR_fyNnM8cAhJbTcL0BKbri7HnaNZU";
-    
-    
-    const run = async (title) => {
-      const genAI = new GoogleGenerativeAI(API_KEY);
-      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-  
-      const generationConfig = {
-        temperature: 0.8,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048,
-      };
-  
-      const safetySettings = [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-      ];
-  
-      const parts = [
-        { text: `rewrite this title: ${title}` }
-      ];
-  
-      const result = await model.generateContent({
-        contents: [{ role: "user", parts }],
-        generationConfig,
-        safetySettings,
-      });
-  
-      const response = result.response;
-      console.log(response.text(), ' and ', title);
-      setFirstItemTitle(response.text());
-    };
-  
-    const runPost = async (postDescription) => {
-      const genAI = new GoogleGenerativeAI(API_KEY);
-      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-  
-      const generationConfig = {
-        temperature: 0.8,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048,
-      };
-  
-      const safetySettings = [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-      ];
-  
-      const parts = [
-        { text: `rewrite this post in a professional way. if there is any quotes, do not change it any way: ${postDescription}` }
-      
-      ];
-  
-      const result = await model.generateContent({
-        contents: [{ role: "user", parts }],
-        generationConfig,
-        safetySettings,
-      });
-  
-      const response = result.response;
-      console.log(response.text(), ' and ', postDescription);
-      setFirstItemPost(response.text());
-    };
-  
-    fetchData().catch((error) => {
-      console.error('Error:', error);
-    });
-
-  }, []);
 
     return (
-    // Use a div element with flex and flex-wrap classes to create a responsive layout
     <div className="flex flex-wrap">
-              <p className="rss-item-content">{firstItemTitle}</p>
-              <p className="rss-item-content">{firstItemPost}</p>
 
       {articles.map((article) => (
-        // Use a div element with w-full and md:w-1/4 classes to make each article take up full width on small screens and one-fourth width on medium screens
         <div className="w-full md:w-1/4 p-4">
           <ArticleCard {...article} />
         </div>
@@ -160,10 +21,9 @@ const MustRead = ({ articles }) => {
   );
 };
 
-// Define a custom component called ArticleCard that takes the image, title, description, and author of an article as props
+
 const ArticleCard = ({ image, title, description, author }) => {
   return (
-    // Use a div element with max-w-sm, bg-white, rounded-xl, and shadow-md classes to create a card-like appearance
     <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden">
       <div className="flex">
         <img
