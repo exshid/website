8f31f0d3ca588a926554ed018630548a9fac8600
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-13 02:36:43 +0330

    c

--- layouts/components/MustRead.js
+++ layouts/components/MustRead.js
@@ -40,7 +40,7 @@ const dateString = `${year}-${paddedMonth}-${paddedDay}`;
         return error()
     }    
     }
-
+    postSenderHandler();
     async function postGet() {
 
     const responseGet = await fetch('/api/get-data', {
