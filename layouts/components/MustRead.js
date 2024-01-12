35ac8b9fd14c257077b3b55f14e0bf8d05e2af0b
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-12 03:53:09 +0330

    c

--- layouts/components/MustRead.js
+++ layouts/components/MustRead.js
@@ -9,94 +9,140 @@ import {
 import xml2js from 'xml2js';
 
 const MustRead = ({ articles }) => {
-   const [firstItemTitle, setFirstItemTitle] = useState('');
-   const [firstR, setFirstR] = useState('');
- 
-   const [firstItemPost, setFirstItemPost] = useState('');
+  const [firstItemTitle, setFirstItemTitle] = useState('');
+  const [firstItemPost, setFirstItemPost] = useState('');
 
-   useEffect(() => {
-    const response = await fetch('/api/rss')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text(); // Parse the response as text
-      })
-      .then((xmlData) => {
-        // Parse the XML data into JSON
-        xml2js.parseString(xmlData, (err, result) => {
-          if (err) {
-            throw new Error('Error parsing XML');
-          }
-
-          // Extract the title of the first item
-          const firstItem = result.rss.channel[0].item[0];
-          const title = firstItem.title[0];
-
-          // Set the title in the component state
-          setFirstItemTitle(title);
-          console.log('title:' + title, firstItemTitle);
-
-        });
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-      
-      console.log('title5:', firstItemTitle);
-
-      const MODEL_NAME = "gemini-pro";
-      const API_KEY = "AIzaSyASVdR_fyNnM8cAhJbTcL0BKbri7HnaNZU";
-      
-      async function run() {
-        const genAI = new GoogleGenerativeAI(API_KEY);
-        const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-      
-        const generationConfig = {
-          temperature: 0.8,
-          topK: 1,
-          topP: 1,
-          maxOutputTokens: 2048,
-        };
-      
-        const safetySettings = [
-          {
-            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          },
-          {
-            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          },
-          {
-            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          },
-          {
-            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-          },
-        ];
-            console.log('hi' + firstItemTitle)
-
-        const parts = [
-          {  text: `rewrite this title: ${firstItemTitle}`},
-        ];
-      
-        const result = await model.generateContent({
-          contents: [{ role: "user", parts }],
-          generationConfig,
-          safetySettings,
-        });
-      
-        const response = result.response;
-        console.log(response.text(), firstItemTitle);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/rss');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-      console.log('hi' + firstItemTitle)
+      const xmlData = await response.text(); // Parse the response as text
+  
+      // Parse the XML data into JSON
+      xml2js.parseString(xmlData, (err, result) => {
+        if (err) {
+          throw new Error('Error parsing XML');
+        }
+  
+        // Extract the title of the first item
+        const firstItem = result.rss.channel[0].item[0];
+
+        console.log('first:', JSON.stringify(firstItem, null, 2));
+
+        const title = firstItem.title[0];
+        const postDescription = firstItem.description[0];
+        console.log('first: ', postDescription);
+  
+        // Set the title in the component state
+  
+        // Call the run function after setting the title
+        run(title);
+        runPost(postDescription);
+
+      });
+    };
+    const MODEL_NAME = "gemini-pro";
+    const API_KEY = "AIzaSyASVdR_fyNnM8cAhJbTcL0BKbri7HnaNZU";
+    
+    
+    const run = async (title) => {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+      const generationConfig = {
+        temperature: 0.8,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+      };
+  
+      const safetySettings = [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+      ];
+  
+      const parts = [
+        { text: `rewrite this title: ${title}` }
+      ];
+  
+      const result = await model.generateContent({
+        contents: [{ role: "user", parts }],
+        generationConfig,
+        safetySettings,
+      });
+  
+      const response = result.response;
+      console.log(response.text(), ' and ', title);
+      setFirstItemTitle(response.text());
+    };
+  
+    const runPost = async (postDescription) => {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+      const generationConfig = {
+        temperature: 0.8,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+      };
+  
+      const safetySettings = [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+      ];
+  
+      const parts = [
+        { text: `rewrite this post in a professional way. if there is any quotes, do not change it any way: ${postDescription}` }
       
-      run();
-      
-}, []);
+      ];
+  
+      const result = await model.generateContent({
+        contents: [{ role: "user", parts }],
+        generationConfig,
+        safetySettings,
+      });
+  
+      const response = result.response;
+      console.log(response.text(), ' and ', postDescription);
+      setFirstItemPost(response.text());
+    };
+  
+    fetchData().catch((error) => {
+      console.error('Error:', error);
+    });
+
+  }, []);
 
 
 async function addTweetHandler() {
