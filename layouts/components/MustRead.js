609ded27252c01dcbba20626811b021ecdbd4e32
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-14 17:25:06 +0330

    ui

--- layouts/components/MustRead.js
+++ layouts/components/MustRead.js
@@ -391,6 +391,49 @@ const dateString = `${year}-${paddedMonth}-${paddedDay}`;
       </ul>
     </div>
     </div>
+    <div className="flex">
+      <div>
+      <div className="bg-white p-4">
+  <div className="font-bold text-black">Kansas City Outlasts Miami in Frigid N.F.L. Wild-Card Matchup</div>
+  <div className="text-gray-500">FROM THE ATHLETIC</div>
+  <div className="text-gray-500">2 MIN READ</div>
+</div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">Kansas City Outlasts Miami in Frigid N.F.L. Wild-Card Matchup</div>
+  <div className="text-gray-500">FROM THE ATHLETIC</div>
+  <div className="text-gray-500">2 MIN READ</div>
+</div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">Kansas City Outlasts Miami in Frigid N.F.L. Wild-Card Matchup</div>
+  <div className="text-gray-500">FROM THE ATHLETIC</div>
+  <div className="text-gray-500">2 MIN READ</div>
+</div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">Leon Wildes, Immigration Lawyer Who Defended John Lennon, Dies at 90</div>
+  <div className="text-gray-500">Battling the government for more than three years, he was able to prove that the Nixon White House was trying to deport the former Beatle for political reasons.</div>
+  <div className="text-gray-500">5 MIN READ</div>
+</div>
+<div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">In Race to Replace George Santos, Financial Questions Re-emerge</div>
+  <div className="text-gray-500">Mazi Pilip, the Republican candidate running in New York’s Third District, drew scrutiny after her initial financial disclosure was missing information.</div>
+  <div className="text-gray-500">4 MIN READ</div>
+</div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">In Race to Replace George Santos, Financial Questions Re-emerge</div>
+  <div className="text-gray-500">Mazi Pilip, the Republican candidate running in New York’s Third District, drew scrutiny after her initial financial disclosure was missing information.</div>
+  <div className="text-gray-500">4 MIN READ</div>
+</div>
+<div className="bg-white p-4">
+  <div className="font-bold text-black">In Race to Replace George Santos, Financial Questions Re-emerge</div>
+  <div className="text-gray-500">Mazi Pilip, the Republican candidate running in New York’s Third District, drew scrutiny after her initial financial disclosure was missing information.</div>
+  <div className="text-gray-500">4 MIN READ</div>
+</div>
+
+</div>
+
+      </div>
+      </div>
 </>  
   );
 };
