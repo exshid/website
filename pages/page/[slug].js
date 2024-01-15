3e22804619721fd7820dac3e1aed648b545061a2
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-15 17:17:05 +0330

    ui

--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -1,38 +1,8 @@
-import Pagination from "@components/Pagination";
-import config from "@config/config.json";
-import Base from "@layouts/Baseof";
-import MustRead from "@layouts/components/MustRead";
-import EditorContainer from "@layouts/components/EditorContainer";
-import EditorPickLeft from "@layouts/components/EditorPickLeft";
-import EditorPickCenter from "@layouts/components/EditorPickCenter";
-import EditorPickRight from "@layouts/components/EditorPickRight";
-import HeadLines from "@layouts/components/HeadLines";
-import CategoryLinks from "@layouts/components/CategoryLinks";
-import DontMissContainer from "@layouts/components/DontMissContainer";
-import DontMiss from "@layouts/components/DontMiss";
-import LatestPostsContainer from "@layouts/components/LatestPostsContainer";
-import LatestPosts from "@layouts/components/LatestPosts";
-import Sidebar from "@layouts/components/Sidebar";
-import Posts from "@partials/Posts";
-const { blog_folder } = config.settings;
-import { articles } from "@layouts/components/articles.js";
-import { categoriesPost } from "@layouts/components/categories.js";
 
 const BlogPagination = () => {
 
   return (
-    <Base>
-    </EditorContainer>
-
-    <LatestPostsContainer>
-      </LatestPostsContainer>
-    <DontMissContainer>
-  
-    </DontMissContainer>
-      <section className="section">
-        <div className="container">
-        </div>
-    </Base>
+<div></div>
   );
 };
 
