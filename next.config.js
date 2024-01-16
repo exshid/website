83ea4f1b304f57eddc7b6cc34d29e5eb1807bf1a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-16 23:59:20 +0330

    commit

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   },
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, child_process: false };
 
     return config;
   },
