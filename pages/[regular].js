b618b074761a04c8f0dc7b2385cecd24c86958c2
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-17 01:51:11 +0330

    commit

--- pages/[regular].js
+++ pages/[regular].js
@@ -2,30 +2,29 @@ import { MongoClient, ObjectId } from 'mongodb'
 import Image from 'next/image'
 import Link from 'next/link';
 
-function Tweet(props) {
-const myCats = JSON.parse(props.rweetData.cats);
+function Tweet(props, relatedPosts ) {
+
+  const myCats = JSON.parse(props.rweetData.cats);
 const myTags = JSON.parse(props.rweetData.tags);
 function formatDate(dateString) {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return new Date(dateString).toLocaleDateString(undefined, options);
 }
-
-
     return <>
          <div id={props.rweetData.id} className="flex flex-col items-center divide-y">
-         <div className="flex flex-row pb-6">
-<div className="w-1/2 flex flex-col justify-center">        
+         <div className="flex flex-row pb-6 w-4/5">
+<div className="w-1/2 flex flex-col justify-center p-3">        
 {myCats.map((item) => (
   <span key={Math.random()} className="text-pink-800 text-lg uppercase">{item}</span>
 ))}
 
             <h1 className="font-bold text-black text-4xl">{props.rweetData.title}</h1>
-            <p className="text-lg">{props.rweetData.description}</p>
+            <p className="text-lg py-3">{props.rweetData.description}</p>
 
   <span className="text-lg uppercase font-bold">By {props.rweetData.author}</span>
   <span className="text-lg">{formatDate(props.rweetData.date)}</span>
             </div>
-        <div className="relative h-calc w-1/2">
+        <div className="relative h-calc w-1/2 p-3">
         <Image
         src={props.rweetData.image} 
         alt={props.rweetData.image}
@@ -47,7 +46,15 @@ function formatDate(dateString) {
           );
         })}
       </div>
- 
+      <h2>Related Posts</h2>
+      {relatedPosts.map((post) => (
+        <div key={post.id}>
+          <Link href={`/posts/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      ))}
+
         </div>
 
         </>
@@ -75,6 +82,7 @@ export async function getStaticPaths() {
 
 
 export async function getStaticProps(context) {
+
   const tweetId = context.params.regular;
 
   const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
@@ -84,6 +92,27 @@ export async function getStaticProps(context) {
   const tweetsCollection = db.collection('rweets');
 
   const rweet = await tweetsCollection.findOne({ _id: new ObjectId(tweetId) })
+
+  async function getRelatedPosts(tags, cats) {
+    
+    // Find posts with at least one matching tag or category
+    const relatedPosts = await tweetsCollection.find({
+      $or: [
+        { tags: { $in: tags } },
+        { cats: { $in: cats } }
+      ]
+    }).toArray()
+  
+    // Convert _id to string for each post
+    relatedPosts.forEach(post => {
+      post._id = post._id.toString()
+    })
+  
+    return relatedPosts
+  }
+
+  // Call getRelatedPosts with the tags and cats of the current post
+  const relatedPosts = await getRelatedPosts(rweet.tags, rweet.cats)
 
   client.close()
   return {
@@ -99,11 +128,10 @@ export async function getStaticProps(context) {
             id: rweet._id.toString(),
             author: rweet.author,
             image: rweet.image
-}
+          },
+          relatedPosts // Include relatedPosts in the props
       }
   }
-
-
 }
 export default Tweet
 
