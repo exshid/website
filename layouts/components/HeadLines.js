b4b885d08e079c0c8b6b4efcc5a25409b597790b
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-18 02:37:06 +0330

    commit

--- layouts/components/HeadLines.js
+++ layouts/components/HeadLines.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const HeadLines = ({ items }) => {
     return (
-      <div className="flex flex-wrap justify-between p-3 md:py-4 md:px-20">
+      <div className="flex flex-wrap justify-between p-3 md:py-4 lg:px-20">
         <h2 className="w-full text-2xl font-bold mb-4">PODCASTS</h2>
         <div className="flex flex-wrap justify-between flex-col lg:flex-row">
           {items.slice(0, 3).map((podcast) => (
