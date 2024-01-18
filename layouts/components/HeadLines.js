0cf9e791dc35e1a8a72a5b3e1630e9b8984987ae
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-18 16:01:11 +0330

    responsive

--- layouts/components/HeadLines.js
+++ layouts/components/HeadLines.js
@@ -9,16 +9,15 @@ const HeadLines = ({ items }) => {
           {items.slice(0, 3).map((podcast) => (
             <div key={Math.random()} className="w-full lg:w-1/3 py-2">
               <div className="py-2 lg:py-4 rounded-lg flex">
-                <div className="px-5">
+                <div className="px-5 w-1/3">
                 <Image src={podcast.image} alt={podcast.title}
                 className="w-28	h-28 relative rounded-sm object-cover mb-2"
                 objectFit="cover"/>
                 </div>
-                <div>
+                <div className="w-2/3">
                 <Link href={podcast.id}>
-
                   <h3 className="text-lg font-bold mb-1">{podcast.title}</h3>
-</Link>
+                </Link>
                   <span className="text-xs text-gray-400 mt-2">{podcast.author}</span>
                 </div>
               </div>
