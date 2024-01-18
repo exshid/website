8cea88bc341e8eea3daa1403c82e1faa721b22a9
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-19 00:17:14 +0330

    commit

--- layouts/components/LatestPosts.js
+++ layouts/components/LatestPosts.js
@@ -23,33 +23,9 @@ const LatestPosts = ({ items }) => {
         {items.map((item) => (
          <>
                {windowWidth > 1024 ? (
-
-         <div key={Math.random()} className="flex flex-col">
-         <Link href={item.id}>
-           <h3 className="font-semibold text-lg md:text-xl lg:text-2xl pt-2">{item.title}</h3>
-           </Link>
-             <div className="flex flex-row-reverse justify-between">
-         <Image src={item.image} alt={item.title}
-         className="object-cover !relative lg:p-4 ml-2 w-28 h-20 md:w-60 md:h-48 lg:w-80 lg:h-64 rounded-m"
-          objectFit="cover"/>
-          <div>
-          <p className="py-2 text-black">
-        {item.description.length > 81 
-          ? `${item.description.substring(0, 81)}...` 
-          : item.description}
-            </p>
-         <div>
-           <span className="font-bold text-black pb-2">{item.author}</span>
-         </div>
-         </div>
-         </div>
-       </div>
-      ) : (
-         
-         <div key={Math.random()}>
+<div key={Math.random()}>
             <Image src={item.image} alt={item.title}
             className="object-cover !relative lg:px-4 lg:py-3 w-28 h-20 md:w-60 md:h-48 lg:w-80 lg:h-64 rounded-m"
-
                     objectFit="cover"/>
             <div>
             <Link href={item.id}>
@@ -59,7 +35,27 @@ const LatestPosts = ({ items }) => {
               <p className="font-bold text-black">{item.author}</p>
             </div>
           </div>
-  )}
+       ) : (
+        <div key={Math.random()} className="flex flex-col">
+        <Link href={item.id}>
+          <h3 className="font-semibold text-lg md:text-xl lg:text-2xl pt-2">{item.title}</h3>
+          </Link>
+            <div className="flex flex-row-reverse justify-between">
+        <Image src={item.image} alt={item.title}
+        className="object-cover !relative lg:p-4 ml-2 w-28 h-20 md:w-60 md:h-48 lg:w-80 lg:h-64 rounded-m"
+         objectFit="cover"/>
+         <div>
+         <p className="py-2 text-black">
+       {item.description.length > 81 
+         ? `${item.description.substring(0, 81)}...` 
+         : item.description}
+           </p>
+        <div>
+          <span className="font-bold text-black pb-2">{item.author}</span>
+        </div>
+        </div>
+        </div>
+      </div> )}
   </>
         ))}
       </div>
