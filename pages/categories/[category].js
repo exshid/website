0b15e33fa76ba767a797d4b744c4b0dbd91d351d
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-19 23:30:02 +0330

    commit

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -26,7 +26,7 @@ const Category = ({ category, posts, authors }) => {
 export default Category;
 
 // category page routes
-export const getStaticPaths = () => {
+export async function getStaticProps() {
   const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
   const db = client.db()
   const tweetsCollection = db.collection('rweets');
