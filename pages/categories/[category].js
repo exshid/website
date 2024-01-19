ae4204ba94ddd5359fe87a175a59917199e06064
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-20 02:09:22 +0330

    commit

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 
 import config from "@config/config.json";
 import Base from "@layouts/Baseof";
