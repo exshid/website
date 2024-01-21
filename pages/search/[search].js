a54701b4482ac619ed2edc6431c4bc2b18034d34
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-21 16:29:58 +0330

    commit

---
+++ pages/search/[search].js
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+
+import config from "@config/config.json";
+import Base from "@layouts/Baseof";
+import { getSinglePage } from "@lib/contentParser";
+import { getTaxonomy } from "@lib/taxonomyParser";
+import { slugify } from "@lib/utils/textConverter";
+import Posts from "@partials/Posts";
+const { blog_folder } = config.settings;
+import { MongoClient } from 'mongodb'
+import LatestPostsContainer from "@layouts/components/LatestPostsContainer";
+import LatestTags from "@layouts/components/LatestTags";
+
+// category page
+const Category = ({ search, posts }) => {
+  if (!posts) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Base title={search}>
+      <div className="section">
+          <h1 className="h2 mb-8 text-center">
+            Showing posts from <span className="text-primary">{search}</span>{" "}
+            search
+          </h1>
+          <LatestPostsContainer>
+         <LatestTags items={posts} />
+            </LatestPostsContainer>
+        </div>
+    </Base>
+  );
+};
+
+export default Category;
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
+  const db = client.db()
+  const tweetsCollection = db.collection('rweets');
+  const rweets = await tweetsCollection.find().toArray()
+  
+  const allCategories = rweets.reduce((acc, tweet) => {
+    const cats = JSON.parse(tweet.cats); // Parse the JSON string into a JavaScript array
+    return acc.concat(cats);
+  }, []);
+  const categories = [...new Set(allCategories)]; // Extract unique categories
+  
+  client.close()
+
+  const paths = allCategories.map((category) => ({
+    params: {
+      category: category,
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+
+export async function getStaticProps({ params }) {
+  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority'); 
+  const db = client.db() 
+  const tweetsCollection = db.collection('rweets');
+  let posts = await tweetsCollection.find().toArray();
+
+    posts = posts.reverse().slice(0, 20).map(post => ({
+    ...post,
+    _id: post._id.toString(),
+  }));
+    
+
+  return {
+    props: { 
+      posts: posts, 
+      search: params.search, 
+    },
+  };
+};
