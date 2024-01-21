2bc7ea0cb1ed7daaf9eb7256afd075e604a591e0
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-21 16:23:25 +0330

    commit

--- pages/search.js
+++ pages/search.js
@@ -1,51 +1,65 @@
 import Base from "@layouts/Baseof";
-import MustRead from "@layouts/components/MustRead";
-import EditorContainer from "@layouts/components/EditorContainer";
-import EditorPickLeft from "@layouts/components/EditorPickLeft";
-import EditorPickCenter from "@layouts/components/EditorPickCenter";
-import EditorPickRight from "@layouts/components/EditorPickRight";
-import HeadLines from "@layouts/components/HeadLines";
-import CategoryLinks from "@layouts/components/CategoryLinks";
-import DontMissContainer from "@layouts/components/DontMissContainer";
-import DontMiss from "@layouts/components/DontMiss";
-import LatestPostsContainer from "@layouts/components/LatestPostsContainer";
-import LatestTags from "@layouts/components/LatestTags";
-import Sidebar from "@layouts/components/Sidebar";
-import Posts from "@partials/Posts";
+import Posts from "@layouts/partials/Posts";
+import { getSinglePage } from "@lib/contentParser";
+import { slugify } from "@lib/utils/textConverter";
+import { useSearchContext } from "context/state";
+import { useRouter } from "next/router";
 
-const SearchPage = ({ articles }) => {
+const SearchPage = ({ authors }) => {
+  const router = useRouter();
+  const { query } = router;
+  const keyword = slugify(query.key);
+  const { posts } = useSearchContext();
+
+  const searchResults = posts.filter((product) => {
+    if (product.frontmatter.draft) {
+      return !product.frontmatter.draft;
+    }
+    if (slugify(product.frontmatter.title).includes(keyword)) {
+      return product;
+    } else if (
+      product.frontmatter.categories.find((category) =>
+        slugify(category).includes(keyword)
+      )
+    ) {
+      return product;
+    } else if (
+      product.frontmatter.tags.find((tag) => slugify(tag).includes(keyword))
+    ) {
+      return product;
+    } else if (slugify(product.content).includes(keyword)) {
+      return product;
+    }
+  });
+
   return (
-    <Base >
+    <Base title={`Search results for ${query.key}`}>
       <div className="section">
+        <div className="container">
           <h1 className="h2 mb-8 text-center">
-            Showing posts from <span className="text-primary"></span>{" "}
-            category
+            Search results for <span className="text-primary">{query.key}</span>
           </h1>
-          <LatestPostsContainer>
-         <LatestTags items={articles} />
-            </LatestPostsContainer>
+          {searchResults.length > 0 ? (
+            <Posts posts={searchResults} authors={authors} />
+          ) : (
+            <div className="py-24 text-center text-h3 shadow">
+              No Search Found
+            </div>
+          )}
         </div>
+      </div>
     </Base>
   );
 };
 
 export default SearchPage;
 
-export async function getStaticProps() {
-  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority'); 
-  const db = client.db() 
-  const tweetsCollection = db.collection('rweets');
-  let posts = await tweetsCollection.find().toArray();
-
-    posts = posts.reverse().slice(0, 20).map(post => ({
-    ...post,
-    _id: post._id.toString(),
-  }));
-    
-
+// get authors data
+export const getStaticProps = () => {
+  const authors = getSinglePage("content/authors");
   return {
-    props: { 
-      posts: posts, 
+    props: {
+      authors: authors,
     },
   };
 };
