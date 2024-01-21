5072785513503a39e52a4247155fb12d7006df04
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-21 16:15:51 +0330

    commit

--- pages/search.js
+++ pages/search.js
@@ -15,54 +15,61 @@ import Posts from "@partials/Posts";
 
 const SearchPage = ({ articles }) => {
   return (
-      <Base>
-        <EditorContainer>
-      <EditorPickCenter items={articles} />
-      <EditorPickRight items={articles} />
-  <EditorPickLeft items={articles} />
-  </EditorContainer>
-  <MustRead items={articles} />
-      <HeadLines items={articles} />
-      <LatestPostsContainer>
-      <LatestPosts items={articles} />
-      <Sidebar items={articles} />
-        </LatestPostsContainer>
-      <DontMissContainer>
-      <DontMiss items={articles} headline="News" length={rweetsLength}/>
-      <DontMiss items={articles} headline="AI" length={rweetsLength}/>
-      </DontMissContainer>
+    <Base >
+      <div className="section">
+          <h1 className="h2 mb-8 text-center">
+            Showing posts from <span className="text-primary"></span>{" "}
+            category
+          </h1>
+          <LatestPostsContainer>
+         <LatestTags items={articles} />
+            </LatestPostsContainer>
+        </div>
+    </Base>
+  );
+};
+
+export default SearchPage;
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
+  const db = client.db()
+  const tweetsCollection = db.collection('rweets');
+  const rweets = await tweetsCollection.find().toArray()
   
-      </Base>
-    );
+  const allCategories = rweets.reduce((acc, tweet) => {
+    const cats = JSON.parse(tweet.cats); // Parse the JSON string into a JavaScript array
+    return acc.concat(cats);
+  }, []);
+  const categories = [...new Set(allCategories)]; // Extract unique categories
+  
+  client.close()
+
+  const paths = allCategories.map((category) => ({
+    params: {
+      category: category,
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority'); 
+  const db = client.db() 
+  const tweetsCollection = db.collection('rweets');
+  let posts = await tweetsCollection.find().toArray();
+
+    posts = posts.reverse().slice(0, 20).map(post => ({
+    ...post,
+    _id: post._id.toString(),
+  }));
+    
+
+  return {
+    props: { 
+      posts: posts, 
+    },
   };
-  
-  export default SearchPage;
-  
-  export async function getStaticProps() {
-    const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
-    const db = client.db()
-    const tweetsCollection = db.collection('rweets');
-    const rweets = await tweetsCollection.find().toArray()
-    client.close()
-    const rweetsLength = [...rweets].length;
-    return {
-        props: {
-          articles: [...rweets].reverse().slice(0, 20).map(rweet => ({
-            author: rweet.author,
-            title: rweet.title,
-            content: rweet.content,
-            description: rweet.description,
-            url: rweet.url,
-            tags: rweet.tags,
-            cats: rweet.cats,
-            date: rweet.date,
-            id: rweet._id.toString(),
-            image: rweet.image
-          })),
-          rweetsLength: rweetsLength
-        },
-        revalidate: 1
-      }
-      
-  }
-  
+};
