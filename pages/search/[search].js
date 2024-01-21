a3bedda1498899072420c1c24ef451e4df158a9a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-21 17:28:26 +0330

    commit

--- pages/search/[search].js
+++ pages/search/[search].js
@@ -13,7 +13,21 @@ const Search = ({ search}) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(dateString).toLocaleDateString(undefined, options);
       }
-        
+      const [isLoading, setIsLoading] = useState(true);
+
+      const [windowWidth, setWindowWidth] = useState(0);
+      
+      useEffect(() => {
+      const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      };
+      
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => {
+      window.removeEventListener('resize', handleResize);
+      };
+      }, []); 
     
     const [mydata, setMydata] = useState(null);
 
@@ -49,6 +63,8 @@ const Search = ({ search}) => {
           </h1>
           <div className="w-full xl:px-20 divide-y p-3">
       {mydata.map((item) => (
+  <>
+  {windowWidth > 1024 ? (
 
 <div key={item._id} className="flex">
   <Image src={item.image} alt={item.title}
@@ -72,6 +88,29 @@ const Search = ({ search}) => {
           </div>
 
      
+     ) : (
+      <div key={Math.random()} className="flex flex-col">
+         <Link href={`/${item._id.toString()}`}>
+
+        <h3 className="font-semibold transition hover:underline text-lg md:text-xl lg:text-3xl pt-2">{item.title}</h3>
+</Link>
+          <div className="flex flex-row-reverse justify-between">
+      <Image src={item.image} alt={item.title}
+      className="object-cover !relative lg:p-4 ml-2 w-28 h-20 md:w-60 md:h-48 lg:w-80 lg:h-64 rounded-m"
+       objectFit="cover"/>
+       <div>
+       <p className="py-2 text-black">
+     {item.description.length > 81 
+       ? `${item.description.substring(0, 81)}...` 
+       : item.description}
+         </p>
+      <div>
+        <span className="font-bold text-black pb-2">{item.author}</span>
+      </div>
+      </div>
+      </div>
+    </div> )}
+</>
       ))}
   </div>
           </div>
