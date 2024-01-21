518d946b7f4e67dd1574d150b6bed28b8bdafc88
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-21 16:06:52 +0330

    commit

--- pages/search.js
+++ pages/search.js
@@ -1,54 +1,68 @@
+import Base from "@layouts/Baseof";
+import MustRead from "@layouts/components/MustRead";
+import EditorContainer from "@layouts/components/EditorContainer";
+import EditorPickLeft from "@layouts/components/EditorPickLeft";
+import EditorPickCenter from "@layouts/components/EditorPickCenter";
+import EditorPickRight from "@layouts/components/EditorPickRight";
+import HeadLines from "@layouts/components/HeadLines";
+import CategoryLinks from "@layouts/components/CategoryLinks";
+import DontMissContainer from "@layouts/components/DontMissContainer";
+import DontMiss from "@layouts/components/DontMiss";
 import LatestPostsContainer from "@layouts/components/LatestPostsContainer";
 import LatestPosts from "@layouts/components/LatestPosts";
-import Base from "@layouts/Baseof";
+import Sidebar from "@layouts/components/Sidebar";
+import Posts from "@partials/Posts";
 
 const SearchPage = ({ articles }) => {
   return (
-
-    <Base title={`Search results for key}`}>
-      <div className="section">
-      <h1 className="h2 mb-8 text-center">
-            Search results for <span className="text-primary"></span>
-          </h1>
-          <LatestPostsContainer>
-
-         <LatestPosts items={articles} />
-          <div className="py-24 text-center text-h3 shadow">
-          No Search Found
-        </div>
-         </LatestPostsContainer>
-        </div>
-    </Base>
-
-  );
-};
-
-export default SearchPage;
-
-
-
-export async function getStaticProps() {
-  const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
-  const db = client.db()
-  const tweetsCollection = db.collection('rweets');
-  const rweets = await tweetsCollection.find().toArray()
-  client.close()
-  return {
-      props: {
-        articles: [...rweets].reverse().map(rweet => ({
-          author: rweet.author,
-          title: rweet.title,
-          content: rweet.content,
-          description: rweet.description,
-          url: rweet.url,
-          tags: rweet.tags,
-          cats: rweet.cats,
-          date: rweet.date,
-          id: rweet._id.toString(),
-          image: rweet.image
-        })),
-        rweetsLength: rweetsLength
-      },
-      revalidate: 1
-    }
- };
+      <Base>
+        <EditorContainer>
+      <EditorPickCenter items={articles} />
+      <EditorPickRight items={articles} />
+  <EditorPickLeft items={articles} />
+  </EditorContainer>
+  <MustRead items={articles} />
+      <HeadLines items={articles} />
+      <LatestPostsContainer>
+      <LatestPosts items={articles} />
+      <Sidebar items={articles} />
+        </LatestPostsContainer>
+      <DontMissContainer>
+      <DontMiss items={articles} headline="News" length={rweetsLength}/>
+      <DontMiss items={articles} headline="AI" length={rweetsLength}/>
+      </DontMissContainer>
+  
+      </Base>
+    );
+  };
+  
+  export default Home;
+  
+  export async function getStaticProps() {
+    const client = await MongoClient.connect('mongodb+srv://ali:Ar7iy9BMcCLpXE4@cluster0.hi03pow.mongodb.net/tweets?retryWrites=true&w=majority')
+    const db = client.db()
+    const tweetsCollection = db.collection('rweets');
+    const rweets = await tweetsCollection.find().toArray()
+    client.close()
+    const rweetsLength = [...rweets].length;
+    return {
+        props: {
+          articles: [...rweets].reverse().slice(0, 20).map(rweet => ({
+            author: rweet.author,
+            title: rweet.title,
+            content: rweet.content,
+            description: rweet.description,
+            url: rweet.url,
+            tags: rweet.tags,
+            cats: rweet.cats,
+            date: rweet.date,
+            id: rweet._id.toString(),
+            image: rweet.image
+          })),
+          rweetsLength: rweetsLength
+        },
+        revalidate: 1
+      }
+      
+  }
+  
