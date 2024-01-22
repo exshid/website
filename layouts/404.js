13fa0a145f671f6734b057a79f0d89d8ea100bb2
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-22 14:45:10 +0330

    Not Found

--- layouts/404.js
+++ layouts/404.js
@@ -1,15 +1,14 @@
-import { markdownify } from "@lib/utils/textConverter";
-
-const NotFound = ({ data }) => {
-  const { frontmatter, content } = data;
+const NotFound = () => {
 
   return (
     <section className="section">
       <div className="container">
         <div className="flex h-[40vh] items-center justify-center">
           <div className="text-center">
-            <h1 className="mb-4">{frontmatter.title}</h1>
-            {markdownify(content, "div", "content prose-headings:text-text")}
+            <h1 className="mb-4">Error 404</h1>
+            <div className="content prose-headings:text-text">
+            Page Not Found
+            </div>
           </div>
         </div>
       </div>
--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,12 @@
 import NotFound from "@layouts/404";
 import Base from "@layouts/Baseof";
 
-const notFound = ({ data }) => {
+const notFound = () => {
   return (
-    <Base>
-      <NotFound data={data} />
+    <Base title={`Not Found - ${title} `}>
+    <NotFound />
     </Base>
   );
-};
-
-// get 404 page data
-export const getStaticProps = async () => {
-  const notFoundData = await getRegularPage("404");
-  return {
-    props: {
-      data: notFoundData,
-    },
-  };
 };
 
 export default notFound;
