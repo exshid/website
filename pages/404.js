732e3cb5fdf3b2907d3d3c9bfda8b221a5d6b2f0
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-22 14:47:30 +0330

    Not Found

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,11 @@
+import config from "@config/config.json";
+
 import NotFound from "@layouts/404";
 import Base from "@layouts/Baseof";
 
 const notFound = () => {
+  const { title } = config.site;
+
   return (
     <Base title={`Not Found - ${title} `}>
     <NotFound />
