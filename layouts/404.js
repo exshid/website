a0b1b50baac1d4680703f2714615a118f1cb7c85
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2024-01-22 14:51:39 +0330

    Not Found

--- layouts/404.js
+++ layouts/404.js
@@ -4,10 +4,10 @@ const NotFound = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="flex h-[40vh] items-center justify-center">
+        <div className="flex min-h-screen items-center justify-center">
           <div className="text-center">
             <h1 className="mb-4">Error 404</h1>
-            <div className="content prose-headings:text-text">
+            <div className="text-lg">
             Page Not Found
             </div>
           </div>
